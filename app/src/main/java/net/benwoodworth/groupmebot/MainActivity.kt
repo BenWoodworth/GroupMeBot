fff0e5f8a51336356dbdd347bfdf85bed321c1f5
BenWoodworth <ben@benwoodworth.net>
2019-04-06 21:31:44 -0400

    Initial commit

---
+++ app/src/main/java/net/benwoodworth/groupmebot/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.benwoodworth.groupmebot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
